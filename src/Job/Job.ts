03976fd5977bb30403b58a1f982876a2747bc8d1
ChaoticWeg <shawn@chaoticweg.cc>
2021-03-21 00:07:17 -0500

    Add child, simplify pid getter

--- src/Job/Job.ts
+++ src/Job/Job.ts
@@ -11,6 +11,7 @@ interface IJob {
     command: string;
     cwd: string;
     pid?: number;
+    child?: ChildProcess;
 }
 
 export class Job extends EventEmitter implements IJob {
@@ -20,15 +21,17 @@ export class Job extends EventEmitter implements IJob {
     };
 
     private _child?: ChildProcess;
+    public get child(): ChildProcess | undefined {
+        return this._child;
+    }
 
     private _command: string;
     public get command(): string {
         return this._command;
     }
 
-    private _pid?: number;
     public get pid(): number | undefined {
-        return this._pid;
+        return this._child?.pid;
     }
 
     private _cwd: string;
