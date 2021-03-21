1d43380f7d5117ce3edd8076f87ae76ea9d44b55
ChaoticWeg <shawn@chaoticweg.cc>
2021-03-21 00:07:24 -0500

    Improve coverage

--- src/Job/Job.test.ts
+++ src/Job/Job.test.ts
@@ -3,29 +3,61 @@ import {Job} from "./Job";
 import path from "path";
 import os from "os";
 
-describe("initialization", () => {
-    const cmd = "echo hi";
+const cmd = "echo test";
 
+describe("initialization", () => {
     test("populates with default options", () => {
         const job = new Job(cmd);
-    
         expect(job.command).toBe(cmd);
         expect(job.cwd).toBe(path.resolve(__dirname));
     });
-    
+
     test("populates with custom cwd", () => {
         const job = new Job(cmd, {cwd: os.tmpdir()});
-    
         expect(job.command).toBe(cmd);
         expect(job.cwd).toBe(os.tmpdir());
     });
+});
 
-    test("runs dummy command", () => {
+describe("run() ", () => {
+    test("success", () => {
         const job = new Job(cmd);
-        try {
+
+        const promise = new Promise<void>((resolve, reject) => {
             job.run();
-        } catch (err) {
-            fail(err);
-        }
+            expect(job.pid).toBeDefined();
+            expect(job.child).toBeDefined();
+
+            job.child?.on("error", (err) => reject(err));
+            job.child?.on("exit", () => resolve());
+        });
+
+        return expect(promise).resolves.toBe(undefined);
+    });
+
+    test("has no pid nor child before run", () => {
+        const job = new Job(cmd);
+        expect(job.child).toBeUndefined();
+        expect(job.pid).toBeUndefined();
+    });
+
+    test("has pid and child after run", () => {
+        const job = new Job(cmd);
+        job.run();
+        expect(job.child).toBeDefined();
+        expect(job.pid).toBeDefined();
+    })
+
+    test("throws on error", () => {
+        // Test with error: echo not defined outside of shell
+        const job = new Job(cmd, {shell: false});
+
+        const promise = new Promise<void>((resolve, reject) => {
+            job.run();
+            job.child?.on("error", (err) => reject(err));
+            job.child?.on("exit", () => resolve());
+        });
+
+        return expect(promise).rejects.toBeDefined();
     });
 });
